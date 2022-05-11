ed60c311084971b597beb59573eb67319a7bbbda
SangNguyen <quangsang41098@gmail.com>
2022-05-12 00:07:36 +0700

    complete

--- test-front-end/src/App.js
+++ test-front-end/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from 'react';
+const App = () => {
+  const dropColor = useRef(null)
+  const randomColor = () => Math.floor(Math.random() * 255);
 
-function App() {
+  const drop = (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("preColor");
+    e.target.style.backgroundColor = data;
+  }
+  const allowDrop = (e) => {
+    dropColor.current = e.target.style.backgroundColor
+    e.preventDefault();
+  }
+  const drag = (e) => {
+    e.dataTransfer.setData("preColor", e.target.style.backgroundColor);
+  }
+  const dragEnd = (e) => {
+    e.target.style.backgroundColor = dropColor.current;
+  }
+  const renderBoard = () => {
+    const keys = Array.from(Array(64).keys());
+    return keys.map(key => {
+      return <div 
+        className='item' 
+        key={key} 
+        id={key}
+        onDrop={drop} onDragOver={allowDrop}
+      >
+        <div 
+          style={{ width: '100%', height: '100%', backgroundColor: `rgba(${randomColor()},${randomColor()},${randomColor()})`}}
+          draggable={true}
+          onDragStart={drag}
+          onDragEnd={dragEnd}
+        >
+
+        </div>
+      </div>
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='items'>{renderBoard()}</div>
     </div>
   );
 }
